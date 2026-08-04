187f732027af7df4ec9f88e8eaaaf284b3baf25b
PARTH DESHMUKH <parthdeshmukh@PARTHs-MacBook-Air.local>
2026-08-04 18:54:01 +0530

    Updates

---
+++ assets/usePageScrollRestoration-CrUyQzF-.js
@@ -0,0 +1,1 @@
+import{C as e,S as t}from"./index-B_LMQWPO.js";var n=e(t(),1),r=new Map;function i(e){let t=(0,n.useRef)(null);return(0,n.useLayoutEffect)(()=>{let n=t.current;if(n)return n.scrollTop=r.get(e)??0,()=>{r.set(e,n.scrollTop)}},[e]),t}export{i as t};
