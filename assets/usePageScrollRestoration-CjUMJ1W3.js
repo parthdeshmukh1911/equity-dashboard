623fc5f3f20776166c45a3f658b001c2516c9737
PARTH DESHMUKH <parthdeshmukh@Mac.lan>
2026-08-15 17:03:05 +0530

    Updates

---
+++ assets/usePageScrollRestoration-CjUMJ1W3.js
@@ -0,0 +1,1 @@
+import{T as e,w as t}from"./index-D8Pf1Jpx.js";var n=e(t(),1),r=new Map;function i(e){let t=(0,n.useRef)(null);return(0,n.useLayoutEffect)(()=>{let n=t.current;if(n)return n.scrollTop=r.get(e)??0,()=>{r.set(e,n.scrollTop)}},[e]),t}export{i as t};
