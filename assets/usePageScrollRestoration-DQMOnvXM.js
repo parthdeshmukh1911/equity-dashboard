a92c8e6747c758dfb9b40cf15791fe57f1dc7151
PARTH DESHMUKH <parthdeshmukh@Mac.lan>
2026-08-22 00:16:57 +0530

    Updates

---
+++ assets/usePageScrollRestoration-DQMOnvXM.js
@@ -0,0 +1,1 @@
+import{A as e,j as t}from"./index-DqgDlFHt.js";var n=t(e(),1),r=new Map;function i(e){let t=(0,n.useRef)(null);return(0,n.useLayoutEffect)(()=>{let n=t.current;if(n)return n.scrollTop=r.get(e)??0,()=>{r.set(e,n.scrollTop)}},[e]),t}export{i as t};
