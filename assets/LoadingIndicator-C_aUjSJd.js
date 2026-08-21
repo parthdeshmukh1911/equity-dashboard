2e0cba6683e5034cb14e9f07885d9739dac49bb1
PARTH DESHMUKH <parthdeshmukh@Mac.lan>
2026-08-22 00:11:37 +0530

    Updates

---
+++ assets/LoadingIndicator-C_aUjSJd.js
@@ -0,0 +1,17 @@
+import{T as e}from"./index-eH32HJCk.js";var t=e();function n({loading:e}){return e?(0,t.jsxs)(`span`,{className:`relative flex h-3 w-3`,children:[(0,t.jsx)(`span`,{className:`
+          absolute
+          inline-flex
+          h-full
+          w-full
+          rounded-full
+          bg-sky-400
+          opacity-60
+          animate-ping
+        `}),(0,t.jsx)(`span`,{className:`
+          relative
+          inline-flex
+          h-3
+          w-3
+          rounded-full
+          bg-sky-500
+        `})]}):null}export{n as t};
